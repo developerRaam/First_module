e3d63c77e24e87e5823d9737ede5a3eb0bb18b04
Ramnarayan <rnm312000@gmail.com>
2023-12-19 00:51:46 +0530

    First Module

---
+++ upload/admin/language/en-gb/extension/module/first_module.php
@@ -0,0 +1,11 @@
+<?php
+
+$_['heading_title']    = 'First Module';
+$_['text_module']      = 'Modules';
+$_['text_success']     = 'Success: You have modified "First Module" module!';
+$_['text_edit']        = 'Edit "First Module" Module';
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_name'] = 'Name';
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify "First Module" module!';
